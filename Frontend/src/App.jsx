b9fdf90cda09e2f6b91221a438f8b0d97605c00a
Santanu <santanuranjanbal@gmail.com>
2026-09-01 11:49:28 +0530

    first commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// Pages
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      {/* Navbar will appear on every page */}
+
+      <Routes>
+        
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default App;
