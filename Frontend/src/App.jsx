f006410b79045aedc32641b08fed68a40dc8c385
roshan-dev12 <pn2279747@gmail.com>
2026-09-04 12:31:13 +0530

    done

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,54 +1,53 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./App.css";
+
+import Topbar from "./Components/Topbar/Topbar";
+import Navbar from "./Components/Navbar/Navbar";
+import Footer from "./Components/footer/footer";
+
+import Home from "./Page/Home/Home";
 import Tours from "./Page/Tours/Tours";
 import TourDetails from "./Page/TourDetails/TourDetails";
 import Hotel from "./Page/Hotel/Hotel";
 import HotelRoomDetails from "./Page/HotelRoomDetails/HotelRoomDetails";
-
-import Navbar from "./Components/Navbar/Navbar";
-
 import SedanCar from "./Page/SedanCar/SedanCar";
 import Suvcars from "./Page/Suvcars/Suvcars";
 import LuxuryCars from "./Page/LuxuryCars/LuxuryCars";
 import TempoTravell from "./Page/TempoTravell/TempoTravell";
 import SmlCoach from "./Page/SmlCoach/SmlCoach";
-import Home from "./Page/Home/Home";
-import Footer from "./Components/footer/footer";
-import About from "./Page/About/About";
 import UrbaniaTraveller from "./Page/UrbaniaTraveller/UrbaniaTraveller";
+import About from "./Page/About/About";
 import Contact from "./Page/Contact/Contact";
 import Faqs from "./Page/Faqs/Faqs";
-
-
-// Pages
 
 const App = () => {
   return (
     <BrowserRouter>
-   
-    <Navbar/>
-     
+      {/* Topbar ऊपर और Navbar ठीक उसके नीचे रहेगा */}
+      <header className="app-header">
+        <Topbar />
+        <Navbar />
+      </header>
 
       <Routes>
-        <Route path="/car-rental/sedan-cars"element={<SedanCar/>}/>
-        <Route path="/car-rental/suv-cars"element={<Suvcars/>}/>
-        <Route path="/car-rental/luxury-cars"element={<LuxuryCars/>}/>
-        <Route path="/car-rental/tempo-travellers"element={<TempoTravell/>}/>
-        <Route path="/car-rental/small-coach"element={<SmlCoach/>}/>
-        <Route path="/" element={<Home/>}/>
-        
-        <Route path="/tours" element={<Tours/>}/>
-        <Route path="/tourdetails" element={<TourDetails/>}/>
-        <Route path="/hotel" element={<Hotel/>}/>
-        <Route path="/hotelroomdetails" element={<HotelRoomDetails/>}/>
-        <Route path="/about" element={<About />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/car-rental/sedan-cars" element={<SedanCar />} />
+        <Route path="/car-rental/suv-cars" element={<Suvcars />} />
+        <Route path="/car-rental/luxury-cars" element={<LuxuryCars />} />
+        <Route path="/car-rental/tempo-travellers" element={<TempoTravell />} />
+        <Route path="/car-rental/small-coach" element={<SmlCoach />} />
         <Route path="/car-rental/urbania-travellers" element={<UrbaniaTraveller />} />
-
-        <Route path="/contact" element={<Contact/>} />
-        <Route path="/faq" element={<Faqs/>}/>
-
+        <Route path="/tours" element={<Tours />} />
+        <Route path="/tourdetails" element={<TourDetails />} />
+        <Route path="/hotel" element={<Hotel />} />
+        <Route path="/hotelroomdetails" element={<HotelRoomDetails />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/faq" element={<Faqs />} />
       </Routes>
-      <Footer/>
+
+      <Footer />
     </BrowserRouter>
   );
 };
