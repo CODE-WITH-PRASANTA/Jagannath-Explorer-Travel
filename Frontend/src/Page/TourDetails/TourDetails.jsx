1a88fb5c9dabf75e939f75c0566aa5174f5d3276
Prasanta Kumar Khuntia <prasantakhuntia2018@gmail.com>
2026-09-20 10:57:10 +0530

    Done

--- Frontend/src/Page/TourDetails/TourDetails.jsx
+++ Frontend/src/Page/TourDetails/TourDetails.jsx
@@ -4,10 +4,10 @@ import API from '../../api/axios';
 
 import TourDetailsBreadCrumb from '../../Components/TourDetailsBreadCrumb/TourDetailsBreadCrumb';
 import TourDetailsPhoto from '../../Components/TourDetailsPhoto/TourDetailsPhoto';
-import TourExperience from '../../Components/Tourexperience/Tourexperience';
 import TourDetailsMap from '../../Components/TourDetailsMap/TourDetailsMap';
 import TourDetailsFaq from '../../Components/TourDetailsFaq/TourDetailsFaq';
 import TourDetailsReview from '../../Components/TourDetailsReview/TourDetailsReview';
+import TourExperiance from '../../Components/TourExperiance/TourExperiance';
 
 const TourDetails = () => {
   const params = useParams();
@@ -96,7 +96,7 @@ const TourDetails = () => {
         videoUrl={tour.videoUrl}
         title={tour.title}
       />
-      <TourExperience
+      <TourExperiance
         tour={tour}
       />
       <TourDetailsMap
