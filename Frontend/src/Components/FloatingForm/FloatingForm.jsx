13b75aacf77a6ee7f28db1d448db1041f4421e98
Akashroul <roulakashkumar2003@gmail.com>
2026-09-21 10:26:34 +0530

    floating form number

--- Frontend/src/Components/FloatingForm/FloatingForm.jsx
+++ Frontend/src/Components/FloatingForm/FloatingForm.jsx
@@ -373,25 +373,25 @@ const FloatingForm = ({
             </p>
 
             {/* Call Button */}
-            <a
-              href="tel:+919583244441"
-              className="floating-form-call-btn"
-              aria-label="Call Jagannath Explorer Travels"
-            >
-              <svg
-                viewBox="0 0 24 24"
-                width="16"
-                height="16"
-                fill="currentColor"
-                aria-hidden="true"
+              <a
+                href="tel:+919668892441"
+                className="floating-form-call-btn"
+                aria-label="Call Jagannath Explorer Travels at +91 9668892441"
               >
-                <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
-              </svg>
+                <svg
+                  viewBox="0 0 24 24"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  aria-hidden="true"
+                >
+                  <path d="M6.62 10.79a15.053 15.053 0 006.59 6.59l2.2-2.2c.27-.27.67-.36 1.02-.24 1.12.37 2.33.57 3.57.57.55 0 1 .45 1 1V20c0 .55-.45 1-1 1-9.39 0-17-7.61-17-17 0-.55.45-1 1-1h3.5c.55 0 1 .45 1 1 0 1.25.2 2.45.57 3.57.11.35.03.74-.25 1.02l-2.2 2.2z" />
+                </svg>
 
-              <span>
-                Call Now: (+91) 9668892441
-              </span>
-            </a>
+                <span>
+                  Call Now: (+91) 9668892441
+                </span>
+              </a>
           </div>
         </div>
 
